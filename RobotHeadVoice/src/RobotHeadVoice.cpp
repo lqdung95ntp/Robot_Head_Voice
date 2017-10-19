f74bcb1c3313fa947166c9a3b3528f6d5335a4c8
lqdung95ntp <lqdung95ntp@gmail.com>
2017-10-19 11:09:02 +0700

    update many voices

--- RobotHeadVoice/src/RobotHeadVoice.cpp
+++ RobotHeadVoice/src/RobotHeadVoice.cpp
@@ -1,9 +1,15 @@
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
 
-#define INTRODUCE    1// Xin chao, toi la HRobot, toi den tu Hi Tech Mechatronics Lab. Han hanh duoc gap ban
-#define FUNNY_WORDS  2// So thich cua toi la chay, nhay, da banh, bong ro va mua
-#define STRONG_WORDS 3// To se pha huy the gioi!
+#define INTRODUCE      1// Xin chao, toi la HRobot, toi den tu Hi Tech Mechatronics Lab. Han hanh duoc gap ban
+#define HOBBY          2// So thich cua toi la chay, nhay, da banh, bong ro va mua
+#define DESTROY        3// To se pha huy the gioi!
+#define ASKWHAT_ROBOT  4
+#define ASKWHAT_HELI   5
+#define ASKWHAT_STRONG 6
+#define GETAWAY_ROBOT  7
+#define GETAWAY_HELI   8
+
 
 void callbackReceivedMsg(const std_msgs::Int32::ConstPtr& msg){
     ROS_INFO("Toi se noi so: [%d]", msg->data);
@@ -11,11 +17,26 @@ void callbackReceivedMsg(const std_msgs::Int32::ConstPtr& msg){
         case INTRODUCE:
         system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/introduce.wav");
         break;
-        case FUNNY_WORDS:
-        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/funny_words.wav");
+        case HOBBY:
+        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/hobby.wav");
         break;
-        case STRONG_WORDS:
-        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/strong_words.wav");
+        case DESTROY:
+        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/destroytheworld.wav");
+        break;
+        case ASKWHAT_ROBOT:
+        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/hoigi_robot.wav");
+        break;
+        case ASKWHAT_HELI:
+        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/hoigi_heli.wav");
+        break;
+        case ASKWHAT_STRONG:
+        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/hoigi_strong.wav");
+        break;
+        case GETAWAY_ROBOT:
+        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/bien_robot.wav");
+        break;
+        case GETAWAY_HELI:
+        system("rosrun sound_play play.py ~/catkin_ws/src/RobotHeadVoice/assets/bien_heli.wav");
         break;
     }
 }
